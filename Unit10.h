371d60e0c2b840fa9344f44783043cbe729ff922
jose1zapata <joseivanzapatar@gmail.com>
2014-03-04 08:01:06 -0430

    dos formas :D

---
+++ Unit10.h
@@ -0,0 +1,22 @@
+//---------------------------------------------------------------------------
+
+#ifndef Unit10H
+#define Unit10H
+//---------------------------------------------------------------------------
+#include <Classes.hpp>
+#include <Controls.hpp>
+#include <StdCtrls.hpp>
+#include <Forms.hpp>
+//---------------------------------------------------------------------------
+class TForm10 : public TForm
+{
+__published:	// IDE-managed Components
+private:	// User declarations
+public:		// User declarations
+        __fastcall TForm10(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm10 *Form10;
+//---------------------------------------------------------------------------
+#endif
+ 
